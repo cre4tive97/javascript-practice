6ba6c970d967c3270c1802a4d7651488ba908a70
harryborrison5148 <harryborrison5148@gmail.com>
2021-09-11 21:57:53 +0900

    input function done

--- js/array.js
+++ js/array.js
@@ -13,9 +13,16 @@ let since = document.querySelector(".array-since");
 let city = document.querySelector(".array-city");
 let food = document.querySelector(".array-food");
 
+let data = [];
 btn.addEventListener("click", function (e) {
   e.preventDefault();
-  let data = [];
-  data.push(since.value);
+  data.push({ since: since.value, city: city.value, food: food.value });
+  let p = document.createElement("p");
   console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    p.innerHTML = `이 가게는 ${data[i].since}년부터 ${data[i].city}에서 ${data[i].food}를 팔고 있어요.`;
+  }
+  document.body.append(p);
 });
+
+// 원본데이터의 보존가능성을 위해서는 map, filter 같은 함수를 사용해 새로운 어레이를 리턴한다.
