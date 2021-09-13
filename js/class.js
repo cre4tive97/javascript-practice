98fad3435ec0aaff9833db69720cffe5879cd1e6
harryborrison5148 <harryborrison5148@gmail.com>
2021-09-13 22:00:56 +0900

    create class.js

---
+++ js/class.js
@@ -0,0 +1,7 @@
+const id = document.querySelector(".class-id");
+const pw = document.querySelector(".class-pw");
+const classBtn = document.querySelector(".class-submit");
+
+classBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+});
